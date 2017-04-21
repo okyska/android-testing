fde2e0edf5db99ee9295eee3921f3f55abaa886a
Blake Matson <blake.a.matson@gmail.com>
2017-04-20 21:30:26 -0700

    adds basic navigation to all available screens via nav drawer

--- app/src/main/java/io/mattcarroll/androidtesting/spending/MonthlySpendingFragment.java
+++ app/src/main/java/io/mattcarroll/androidtesting/spending/MonthlySpendingFragment.java
@@ -1,25 +1,38 @@
 package io.mattcarroll.androidtesting.spending;
 
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import java.util.List;
 
 import io.mattcarroll.androidtesting.R;
 
-public class MonthlySpendingActivity extends AppCompatActivity {
+public class MonthlySpendingFragment extends Fragment {
 
     private MonthlySpendingChartPresenter chartPresenter;
     private RecyclerView recyclerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_monthly_spending);
+    @NonNull
+    public static MonthlySpendingFragment newInstance() {
+        return new MonthlySpendingFragment();
+    }
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_monthly_spending, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) view.findViewById(R.id.recyclerview);
 
         chartPresenter = new MonthlySpendingChartPresenter();
         List<ChartSectionViewModel> chartSectionViewModels = chartPresenter.presentChart();
@@ -31,8 +44,7 @@ public class MonthlySpendingActivity extends AppCompatActivity {
 
         recyclerView.setAdapter(adapter);
 
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         recyclerView.setLayoutManager(layoutManager);
     }
-
 }
--- app/src/main/java/io/mattcarroll/androidtesting/transactions/TransactionListFragment.java
+++ app/src/main/java/io/mattcarroll/androidtesting/transactions/TransactionListFragment.java
@@ -1,17 +1,13 @@
 package io.mattcarroll.androidtesting.transactions;
 
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
-
-import java.util.Arrays;
 
 import io.mattcarroll.androidtesting.R;
 
