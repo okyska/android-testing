779d1e2baeed993b6126baaa31f6a43fe6c7e563
Blake Matson <blake.a.matson@gmail.com>
2017-04-25 23:08:00 -0700

    adds espresso intents example for testing SignUpActivity results

---
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoLoginIntentsTest.java
@@ -0,0 +1,98 @@
+package io.mattcarroll.androidtesting;
+
+
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.support.annotation.NonNull;
+import android.support.test.espresso.intent.OngoingStubbing;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import io.mattcarroll.androidtesting.login.LoginActivity;
+import io.mattcarroll.androidtesting.signup.SignUpActivity;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.intent.Intents.intended;
+import static android.support.test.espresso.intent.Intents.intending;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+public class EspressoLoginIntentsTest {
+    private static final Instrumentation.ActivityResult SUCCESS =
+            createResultWithCode(Activity.RESULT_OK);
+    private static final Instrumentation.ActivityResult SIGN_UP_FAILED =
+            createResultWithCode(LoginActivity.RESULT_SIGN_UP_FAILED);
+    private static final Instrumentation.ActivityResult CANCELLED =
+            createResultWithCode(Activity.RESULT_CANCELED);
+
+    @Rule
+    public final IntentsTestRule<LoginActivity> intentsRule =
+            new IntentsTestRule<>(LoginActivity.class, false, true);
+
+    @Test
+    public void loginActivityShouldLaunchSignUpActivityWhenSignUpPressed() {
+        clickSignUp();
+
+        // Verify SignUpActivity intent is fired
+        intended(hasComponent(SignUpActivity.class.getName()));
+    }
+
+    @Test
+    public void loginActivityShouldShowSuccessDialogWhenSignUpSucceeds() {
+        intendingActivity(SignUpActivity.class).respondWith(SUCCESS);
+
+        clickSignUp();
+
+        // Verify the success dialog is shown
+        onView(withText(R.string.dialog_title_signup_successful))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.dialog_message_signup_successful))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void loginActivityShouldShowFailureDialogWhenSignUpFails() {
+        intendingActivity(SignUpActivity.class).respondWith(SIGN_UP_FAILED);
+
+        clickSignUp();
+
+        // Verify the failure dialog is shown
+        onView(withText(R.string.dialog_title_signup_failed))
+                .check(matches(isDisplayed()));
+        onView(withText(R.string.dialog_message_signup_failed))
+                .check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void loginActivityShouldShowLoginScreenWhenSignUpCancelled() {
+        intendingActivity(SignUpActivity.class).respondWith(CANCELLED);
+
+        clickSignUp();
+
+        // Verify the login screen is now visible
+        onView(withId(R.id.email_login_form))
+                .check(matches(isDisplayed()));
+    }
+
+    @NonNull
+    private static Instrumentation.ActivityResult createResultWithCode(int resultCode) {
+        return new Instrumentation.ActivityResult(resultCode, null);
+    }
+
+    @NonNull
+    private OngoingStubbing intendingActivity(Class<?> activityClass) {
+        return intending(hasComponent(activityClass.getName()));
+    }
+
+    private void clickSignUp() {
+        onView(withId(R.id.sign_up_button))
+                .perform(scrollTo(), click());
+    }
+}
--- app/src/main/java/io/mattcarroll/androidtesting/login/LoginActivity.java
+++ app/src/main/java/io/mattcarroll/androidtesting/login/LoginActivity.java
@@ -1,8 +1,10 @@
 package io.mattcarroll.androidtesting.login;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 
 import io.mattcarroll.androidtesting.Bus;
@@ -13,6 +15,7 @@ import io.mattcarroll.androidtesting.signup.SignUpActivity;
  * Launch-point for a login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
+    public static final int RESULT_SIGN_UP_FAILED = 1001;
 
     private static final int REQUEST_SIGN_UP = 1000;
 
@@ -46,13 +49,40 @@ public class LoginActivity extends AppCompatActivity {
         if (REQUEST_SIGN_UP == requestCode) {
             if (RESULT_OK == resultCode) {
                 setResult(RESULT_OK);
-                finish();
-            } else {
-                // TODO:
+                showSuccessDialog();
+            } else if (SignUpActivity.RESULT_FAILED == resultCode) {
+                setResult(RESULT_SIGN_UP_FAILED);
+                showFailureDialog();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void showSuccessDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_title_signup_successful)
+                .setMessage(R.string.dialog_message_signup_successful)
+                .setNegativeButton(R.string.button_ok, null)
+                .setCancelable(false)
+                .setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialog) {
+                        finish();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+    private void showFailureDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.dialog_title_signup_failed)
+                .setMessage(R.string.dialog_message_signup_failed)
+                .setNegativeButton(R.string.button_ok, null)
+                .setCancelable(false)
+                .create()
+                .show();
     }
 
     @SuppressWarnings("unused")
