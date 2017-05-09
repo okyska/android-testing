d6c9a25160db9740921d22c44134cb20e3094e13
Blake Matson <blake.a.matson@gmail.com>
2017-05-09 00:31:02 -0700

    adds empty test suite

---
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/EspressoSignUpTest.java
@@ -0,0 +1,21 @@
+package io.mattcarroll.androidtesting;
+
+import org.junit.Test;
+
+public class EspressoSignUpTest {
+
+    @Test
+    public void userSignUpHappyPath() {
+
+    }
+
+    @Test
+    public void requiredFieldsAreRequired() {
+
+    }
+
+    @Test
+    public void atLeastOneInterestMustBeSelectedToContinue() {
+
+    }
+}
