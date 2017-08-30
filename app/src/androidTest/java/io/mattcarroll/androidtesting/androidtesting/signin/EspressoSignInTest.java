fbfed533c35b980f46927033c6328b99f343d684
okyska <okyska@yahoo.com>
2017-08-30 11:22:10 -0700

    add test userSignInPersonalInfoVerify

--- app/src/androidTest/java/io/mattcarroll/androidtesting/androidtesting/signin/EspressoSignInTest.java
+++ app/src/androidTest/java/io/mattcarroll/androidtesting/androidtesting/signin/EspressoSignInTest.java
@@ -15,16 +15,21 @@ import io.mattcarroll.androidtesting.androidtesting.SignUpActivity;
 import static android.R.id.edit;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.hasErrorText;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static io.mattcarroll.androidtesting.R.id.edittext_email;
+import static io.mattcarroll.androidtesting.R.id.withText;
+import static org.junit.matchers.JUnitMatchers.containsString;
 
 /**
  * Created by oxana on 8/29/2017.
  */
 
-public class EspressoSignInTestFailedPasswordRequired {
+public class EspressoSignInTest {
 
     @Rule
     public final ActivityTestRule<SplashActivity> activityRule =
@@ -47,5 +52,18 @@ public class EspressoSignInTestFailedPasswordRequired {
                 .check(matches(hasErrorText("This field is required")));
         onView(withId(R.id.edittext_password))
                 .check(matches(hasErrorText("This field is required")));
+    }
+
+    @Test
+    public void userSignInPersonalInfoVerify() {
+        onView(withId(R.id.edittext_email))
+                .perform(typeText("@"));
+        onView(withId(R.id.edittext_password))
+                .perform(typeText("12345"));
+        onView(withId(R.id.button_sign_in))
+                .perform(click());
+        onView(withId(R.id.textview_no_accounts))
+               .check(matches(isDisplayed()));
+
     }
 }
